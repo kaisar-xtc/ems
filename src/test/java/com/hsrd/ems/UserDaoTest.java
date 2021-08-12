6dc66b10e2f364ad1ba2b678f7e807483fcf8aff
kaisar-xtc <88704769+kaisar-xtc@users.noreply.github.com>
2021-08-12 10:52:06 +0800

    pull commit

--- src/test/java/com/hsrd/ems/UserDaoTest.java
+++ src/test/java/com/hsrd/ems/UserDaoTest.java
@@ -21,5 +21,6 @@ public class UserDaoTest{
         System.out.println("master merge test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
